a9b4138a747973068495058fc9e8e613a29cd77f
eritque0arcus <eritque-arcus@eritque-arcus.tech>
2023-10-28 12:49:48 -0400

    feat: add structures

---
+++ data/structures.py
@@ -0,0 +1,22 @@
+class RoadEngager:
+    speed: (float, float)
+    position: (float, float, float)
+    id: str
+
+class ReferenceLine:
+    s: float
+    x: float
+    y: float
+    hdg: float
+    length: float
+
+class RoadType:
+    max_speed: float
+    width: float
+    road_id: str
+    road_type: str
+    country: str
+
+class Map:
+    reference_line: ReferenceLine
+    road_type: RoadType
