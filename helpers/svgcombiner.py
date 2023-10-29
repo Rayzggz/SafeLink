699d38f1be6fe29dbd1add727730a938f404befe
eritque0arcus <eritque-arcus@eritque-arcus.tech>
2023-10-29 01:44:41 -0400

    fix: fix gif problem

--- helpers/svgcombiner.py
+++ helpers/svgcombiner.py
@@ -3,9 +3,8 @@ from pathlib import Path
 import os
 
 from config import CACHE_PATH
-from imageio import imopen
-import imageio
-
+from PIL import Image
+import glob
 
 def svg_combine(base: Path, svg: Path) -> str:
     with open(base, "r") as f:
@@ -26,17 +25,20 @@ def svg_combine(base: Path, svg: Path) -> str:
 
 
 def svg_to_gif(svg_paths, gif_path):
-    png_paths = [svg_paths / a for a in os.listdir(svg_paths) if a.endswith(".svg")]
     
     os.system("cd " + str(CACHE_PATH.absolute()) + " && for file in *.svg; do inkscape \"$file\" -o \"${file%svg}png\"; done")
-    # 读取所有的PNG图片，并存储到images列表中
-    images = [imopen(png_path, 'r') for png_path in png_paths]
 
-    # 将所有图片拼接成gif
-    imageio.mimsave(gif_path, images)
+    # Create the frames
+    frames = []
+    imgs = glob.glob("*.png", root_dir=svg_paths)
+    for i in imgs:
+        new_frame = Image.open(CACHE_PATH / i)
+        frames.append(new_frame)
 
-    # 删除临时图片
-    for png_path in png_paths:
-        os.remove(png_path)
+    # Save into a GIF file that loops forever
+    frames[0].save(CACHE_PATH / 'png_to_gif.gif', format='GIF',
+                append_images=frames[1:],
+                save_all=True,
+                duration=300, loop=0)
 
     print(f'Successfully saved the gif to {gif_path}.')
