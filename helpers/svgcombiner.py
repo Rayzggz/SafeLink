161b41219bdcf73da38eef999af82eeb314b1a1e
Roi Feng <37480123+Rayzggz@users.noreply.github.com>
2023-10-29 05:34:51 -0400

    svgcombiner add alert

--- helpers/svgcombiner.py
+++ helpers/svgcombiner.py
@@ -6,7 +6,7 @@ from config import CACHE_PATH
 from PIL import Image
 import glob
 
-def svg_combine(base: Path, svg: Path) -> str:
+def svg_combine(base: Path, svg: Path, isAlert: bool) -> str:
     with open(base, "r") as f:
         base = f.read()
     with open(svg, "r") as f:
@@ -16,6 +16,13 @@ def svg_combine(base: Path, svg: Path) -> str:
     head = lambda x: """<?xml version="1.0" encoding="utf-8" ?>""" +re.search("""<svg(.*?)">""", x, re.DOTALL)[0]
     bgc = re.search("""<rect fill="#eee" height="(.*)" width="(.*)" x="0.0" y="0.0" />""", svg, re.DOTALL)[0]
     tail = "</svg>"
+
+    alert = """<path class="st0" d="M103.2,62.5c0-24.2-20.4-43.7-45.6-43.7S11.9,38.3,11.9,62.5s20.4,43.7,45.6,43.7S103.2,86.6,103.2,62.5
+	L103.2,62.5L103.2,62.5L103.2,62.5L103.2,62.5L103.2,62.5z M17.3,62.5c0-21.3,18-38.6,40.3-38.6s40.3,17.3,40.3,38.6
+	s-18,38.6-40.3,38.6S17.3,83.8,17.3,62.5L17.3,62.5L17.3,62.5L17.3,62.5L17.3,62.5L17.3,62.5z" fill="#FC5143"/>
+<path class="st0" d="M53.8,73.6c0,2.7,1.3,4.1,3.8,4.1c2.5,0,3.8-1.4,3.8-4.1V35.7c0-2.7-1.3-4.1-3.8-4.1c-2.5,0-3.8,1.4-3.8,4.1
+	V73.6z M53.1,90.3c0.2,2.7,1.7,4.1,4.4,4.1c2.7,0,4.2-1.4,4.4-4.1c-0.2-2.7-1.7-4.2-4.4-4.4C54.8,86.1,53.4,87.6,53.1,90.3
+	L53.1,90.3z" fill="#FC5143"/>"""
 
     # Find the index where </g><text starts in the base
     # index = base.find('</g><text')
@@ -27,8 +34,13 @@ def svg_combine(base: Path, svg: Path) -> str:
     # Insert the svg data at the found index
     # combined = base[:index] + match.group(0)  + base[index:]
     # return combined
+
     if bgc:
+        if isAlert:
+            return head(base) + body(base) + body(svg).replace(bgc, "") + alert + tail
         return head(base) + body(base) + body(svg).replace(bgc, "") + tail
+    if isAlert:
+        return head(base) + body(base) + body(svg) + alert + tail
     return head(base) + body(base) + body(svg) + tail
 
 
