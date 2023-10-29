b39b6ca15e3bd7f14e383b591b4eba51b1b6ef92
Roi Feng <37480123+Rayzggz@users.noreply.github.com>
2023-10-28 23:12:53 -0400

    new svgcombiner.py

---
+++ helpers/svgcombiner.py
@@ -0,0 +1,20 @@
+import re
+from pathlib import Path
+
+
+def svgcombiner(base: Path, svg: Path):
+    with open(base, "r") as f:
+        base = f.read()
+    with open(svg, "r") as f:
+        svg = f.read()
+
+    # Find the index where </g><text starts in the base
+    index = base.find('</g><text')
+
+    match = re.search('(<p.*\/p>)', svg, re.DOTALL)
+
+    match.group(0)
+    # Insert the svg data at the found index
+    combined = base[:index] + "<p" + svg + "/p>" + base[index:]
+
+    return combined
